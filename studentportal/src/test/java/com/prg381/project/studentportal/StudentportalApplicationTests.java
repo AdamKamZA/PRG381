8e813def5a26144dd836bd5ffa1ae1bef5dcb356
Adam Kaminski <71768841+AdamKamZA@users.noreply.github.com>
2022-08-18 08:56:34 +0200

    Spring.io initilisation files

---
+++ studentportal/src/test/java/com/prg381/project/studentportal/StudentportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prg381.project.studentportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
