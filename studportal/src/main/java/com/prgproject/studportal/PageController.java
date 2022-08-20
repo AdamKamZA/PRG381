ef398310fb43cf06d8f6ee2a0d8e926480d134c0
Adam Kaminski <arturkars01@gmail.com>
2022-08-20 10:16:59 +0200

    new base files and compiled jar test - includes login page styling

---
+++ studportal/src/main/java/com/prgproject/studportal/PageController.java
@@ -0,0 +1,15 @@
+package com.prgproject.studportal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    
+    //Routing for home page:
+    @GetMapping("/")
+    public String index(){
+        return "index"; //returning static index html page
+    }
+
+}
