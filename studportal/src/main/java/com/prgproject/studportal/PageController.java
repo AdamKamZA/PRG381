57d7ff376604b45f0176ed6446398de758f06dcb
Adam Kaminski <arturkars01@gmail.com>
2022-08-22 20:27:29 +0200

    Thymeleaft attempt - requires db objects to add to model

--- studportal/src/main/java/com/prgproject/studportal/PageController.java
+++ studportal/src/main/java/com/prgproject/studportal/PageController.java
@@ -1,23 +1,36 @@
 package com.prgproject.studportal;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-    
-    //Routing for home page:
+
+    class Student{
+        String name="Adam";
+        String surname="Kaminski";
+        public Student(){}
+    }
+    // Routing for home page:
     @GetMapping("/")
-    public String index(){
-        return "index"; //returning static index html page
+    public String index() {
+        return "index"; // returning static index html page
     }
+
     @GetMapping("/student")
-    public String student(){
-        return "student"; //returning static student html page
+    public String student(Model model) {
+        Student students = new Student();
+        model.addAttribute("student", students);
+        return "student"; // returning static student html page
     }
+
     @GetMapping("/admin")
-    public String admin(){
-        return "admin"; //returning static admin html page
+    public String admin(Model model) { // Model model
+        // fetch all students
+        Student students = new Student();
+        model.addAttribute("student", students);
+        return "admin"; // returning static admin html page
     }
 
 }
