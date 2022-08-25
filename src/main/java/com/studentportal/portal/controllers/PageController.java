5d428e01b99793a58b5d274ae5fba076b3bde43e
Adam Kaminski <arturkars01@gmail.com>
2022-08-25 11:11:31 +0200

    Login page authentication

--- src/main/java/com/studentportal/portal/controllers/PageController.java
+++ src/main/java/com/studentportal/portal/controllers/PageController.java
@@ -2,36 +2,53 @@ package com.studentportal.portal.controllers;
 
 import java.util.ArrayList;
 
-import org.apache.tomcat.util.json.JSONParser;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-
 import com.studentportal.portal.apiStudent;
+import com.studentportal.portal.services.StudentServiceImpl;
 
 @Controller
 public class PageController {
 
+    StudentServiceImpl studImpl;
+
     // Routing for home page:
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("studImp", studImpl);
         return "index"; // returning static index html page
     }
 
     @GetMapping("/student")
-    public String student(@RequestParam(value = "email", required = true) String email, Model model) {
+    public String student(@RequestParam(value = "email", required = true) String email,
+            @RequestParam(value = "password", required = true) String password, Model model) {
         // will be replaced with student details of the object, was only a test
-        String uri = String.format("http://localhost:8080/api/students/email/%s", email);
-        RestTemplate rt = new RestTemplate();
-        apiStudent student = rt.getForObject(uri, apiStudent.class);
-        JSONObject JSON_Student = new JSONObject(student.toString());
-
-        model.addAttribute("student", JSON_Student);
-        return "student"; // returning static student html page
+        try {
+            String uri = String.format("http://localhost:8080/api/students/email/%s", email);
+            RestTemplate rt = new RestTemplate();
+            apiStudent student = rt.getForObject(uri, apiStudent.class);
+            JSONObject JSON_Student = new JSONObject(student.toString());
+            
+            //Check if the passwords match
+            if (!JSON_Student.get("password").toString().equals(password)) {
+                model.addAttribute("state", false); // tells page if its first time render or a redirect back
+                return "index";
+            }
+            
+            //Routing to the offficial student page, where they can update their password
+            model.addAttribute("student", JSON_Student);
+            return "student"; // returning static student html page
+        } catch (HttpClientErrorException e) {
+            //Redirect, as this means that bad credentials were entered
+            model.addAttribute("state", false); // tells page if its first time render or a redirect back
+            return "index";
+        }
     }
 
     @GetMapping("/admin")
@@ -42,7 +59,7 @@ public class PageController {
         RestTemplate rt = new RestTemplate();
         apiStudent[] arr_Students = rt.getForObject(uri, apiStudent[].class);
         ArrayList<JSONObject> JSON_ARR_Students = new ArrayList<JSONObject>();
-        for(apiStudent student:arr_Students){
+        for (apiStudent student : arr_Students) {
             JSONObject JSON_Student = new JSONObject(student.toString());
             JSON_ARR_Students.add(JSON_Student);
         }
